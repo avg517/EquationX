89eebff0cd6edd83eea3b98c8862d5985b91f193
avg517 <dragosjocuri@gmail.com>
2026-03-18 12:53:50 +0200

    updated the documentation and kinda fixed render.c

--- src/gui/terminal/render.c
+++ src/gui/terminal/render.c
@@ -3,13 +3,17 @@
 #include <stdlib.h>
 #include "terminal-constants.h"
 
+unsigned char* generate_graphics(){
+    unsigned char* graphics =(unsigned char*) malloc(257*sizeof(char));
+    for(int j=0x1;j<=0xFF;j+=0x1){
+        graphics[j]=j;
+    }
+    return graphics;
+}
 void init_ncurses(){//initialisez ncurses
     initscr();
-    cbreak();
     noecho();
-    refresh();
-
-
+    cbreak();
 }
 char** init_buffer(){
     char** buffer = (char**) malloc((1+COLS)*sizeof(char));
@@ -33,7 +37,7 @@ char** render_line(int x1,int y1,int x2,int y2){//the function returns a matrix 
 
     char** pixels =(char**) malloc(2*sizeof(char));
     pixels[0]=(char*) malloc((1+dx)*sizeof(char));//the first vector stores x value// but in the first position it will store the size of the vector
-    pixels[1]=(char*) malloc((1+dx)*sizeof(char));//the second one stores y value
+    pixels[1]=(char*) malloc((1+dy)*sizeof(char));//the second one stores y value
     int i=0;
     for(i=1;x<=x2;i++){
         pixels[0][i]=x;
@@ -46,19 +50,19 @@ char** render_line(int x1,int y1,int x2,int y2){//the function returns a matrix 
             y++;
         }
     }
-    pixels[0][0]=i;
+    pixels[0][0]=i-1;
     return pixels;
 
 }
 
 bool render_sprite(int x,int y,char** sprite,char** buffer){//this function renders the line from the render_line function,but the previous one only computes the pixels you need to "light up",this one puts it on the video buffer at some coordonates you specify
     for(int i=1;i<=sprite[0][0];i++){
-        int x=2;//sprite[0][i];
-        int y=2;//sprite[1][i];
-        if(x>COLS || y > LINES){
+        int x=sprite[0][i];
+        int y=sprite[1][i];
+        if(x>=COLS || y >= LINES){
             return false;//returns false when the line is outside of the screen(but it doesn't mean it should stop the program,only to not continue rendering the line)
         }else{
-            buffer[x][y]= LINE;
+            buffer[x][y]=LINE;
         }
 
 
@@ -66,15 +70,29 @@ bool render_sprite(int x,int y,char** sprite,char** buffer){//this function rend
     return true;
 }
 
-void render_buffer(char** buffer){//the buffer needs to be the size of the window for this to work
+
+
+void render_buffer(char** buffer,unsigned char* graphics){//the buffer needs to be the size of the window for this to work
     //a solution would be to make a very large buffer (for a terminal screen,but in memory it won't be that big)
 
-
+    refresh();
     //void getmaxyx(WINDOW *win, int y, int x); //this function gets the maximum size of the terminal
     //also,this variables are initiliazed by ncurses after initializing the screen: COLS, LINES
-    for(int x=0;x<COLS;x++){
-        for(int y=0;y<LINES;x++){
-            mvaddch(y,x,buffer[x][y]);
+
+    //buffer[2][3]='#';
+    move(1,0);
+    for(int y=0;y<=LINES-1;y++){
+        for(int x=0;x<=COLS;x++){
+            //mvaddch(y,x,buffer[x][y]);
+            //addch(graphics[buffer[x][y]]);
+            addch(buffer[x][y]);
+            //addch(graphics[34]);
+            //addch('#');
+            //if(x==LINES-1){addch('s');}
+            if(x==LINES){
+                //addch('O');
+                move(y+1,0);
+            }
         }
     }
     refresh();
@@ -84,3 +102,4 @@ void stop_ncurses(){
     refresh();
     endwin();
 }
+
