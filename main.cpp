237ca03069e1600cedce006a1db920e2c9de3ae2
avg517 <dragosjocuri@gmail.com>
2026-04-15 15:42:12 +0300

    started adding command line arguments sugestions for the program

--- main.cpp
+++ main.cpp
@@ -9,8 +9,13 @@
 #include "src/graph-plotting/graph_generator.hpp"
 #include "src/gui/terminal/render.h"
 
+/*COMMAND LINE ARGUMENTS:
+-h --help           prints the help screen
+-r --render <file>  renders a function defined in a file
+*/
 
-int main(){
+
+int main(int argc, char *argv[]){
 
 
 }
