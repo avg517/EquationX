fdf2bd63f06ccdd03b46f621155679e4b64865bf
avg517 <dragosjocuri@gmail.com>
2026-04-15 12:59:12 +0300

    included all the files necessary in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
 //this is the main cpp program file used to link all together
+
+#include "src/basefunctions.cpp"
+#include "src/expressfunc.cpp"
+#include "src/functionclass.cpp"
+#include "src/includethemall.cpp"
+#include "src/parser.cpp"
+#include "src/simplecalculator.cpp"
+#include "src/graph-plotting/graph_generator.cpp"
+#include "src/gui/terminal/render.c"
+
+
+int main(){
+
+    
+}
